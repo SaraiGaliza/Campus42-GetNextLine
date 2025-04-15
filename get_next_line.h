c5f45c71282eccb7b278fff01060882f8122274c
Sarai Rodriguez Lago <sarrodri@cbr4s6.42barcelona.com>
2025-04-15 16:51:26 +0200

    a

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarrodri <sarrodri@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/15 15:27:00 by sarrodri          #+#    #+#             */
+/*   Updated: 2025/04/15 15:57:29 by sarrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+// Si no se define externamente, define BUFFER_SIZE como 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// Librerías necesarias
+# include <unistd.h>  // read()
+# include <stdlib.h>  // malloc(), free()
+# include <fcntl.h>   // open(), flags como O_RDONLY
+# include <stdio.h>   // opcional: para printf o pruebas
+
+// Declaración de la función principal
+char	*get_next_line(int fd);
+
+#endif
