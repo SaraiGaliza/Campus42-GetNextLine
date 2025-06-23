6bf276616709658ed5d681c504aebf7f4cecca67
SaraiGaliza <124932608+SaraiGaliza@users.noreply.github.com>
2025-06-23 16:12:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,38 +6,38 @@
 /*   By: sarrodri <sarrodri@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 17:21:25 by sarrodri          #+#    #+#             */
-/*   Updated: 2025/06/18 17:22:55 by sarrodri         ###   ########.fr       */
+/*   Updated: 2025/06/23 14:55:46 by sarrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <fcntl.h> /* open, O_RDONLY */
+#include <stdio.h> /* printf, perror */
+#include <stdlib.h> /* free, exit */
+#include <unistd.h> /* close */
 
 int	main(int argc, char **argv)
 {
-	int		fd;
-	char	*line;
+	int		fd; /* Descriptor de archivo que usaremos */
+	char	*line; /* Puntero donde get_next_line devuelve cada línea */
 
-	if (argc < 2)
-		fd = 0;
-	else
+	if (argc < 2) // No hay argumento
+		fd = 0; // Usamos stdin
+	else // Hay argumento
 	{
-		fd = open(argv[1], O_RDONLY);
+		fd = open(argv[1], O_RDONLY); // Lo abrimos en modo lectura
 		if (fd < 0)
 		{
-			perror("open");
+			perror("open"); // Si falla imprimimos el error
 			return (1);
 		}
 	}
 	while ((line = get_next_line(fd)))
 	{
-		printf("%s", line);
-		free(line);
+		printf("%s", line); // Imprimimos linea 
+		free(line); // Liberamos memoria
 	}
-	if (argc >= 2)
-		close(fd);
+	if (argc >= 2) // Si abrimos un fichero
+		close(fd); // Lo cerramos
 	return (0);
 }
